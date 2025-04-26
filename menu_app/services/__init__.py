9ee6eef215c0fc868f710886894561f1de8b7c68
silentli <heylibai@gmail.com>
2025-04-26 22:45:26 +0200

    refactor(inventory): update inventory_service

---
+++ menu_app/services/__init__.py
@@ -0,0 +1,5 @@
+from menu_app.services.menu_service import MenuService
+from menu_app.services.order_service import OrderService
+from menu_app.services.inventory_service import InventoryService
+
+__all__ = ['MenuService', 'OrderService', 'InventoryService'] 
